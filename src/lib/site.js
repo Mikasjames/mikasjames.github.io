17e1dd883f315660c654f8ff5a9f7948f15131d9
Mikas James <mikasjames@gmail.com>
2026-06-08 17:05:57 +0800

    feat: rename public site URL from .is-a.dev to .com

--- src/lib/site.js
+++ src/lib/site.js
@@ -2,5 +2,5 @@ import { env } from "$env/dynamic/public";
 
 /** @type {string} */
 export const SITE_URL = (
-  env.PUBLIC_SITE_URL ?? "https://mikasjames.is-a.dev"
+  env.PUBLIC_SITE_URL ?? "https://mikasjames.com"
 ).replace(/\/$/, "");
